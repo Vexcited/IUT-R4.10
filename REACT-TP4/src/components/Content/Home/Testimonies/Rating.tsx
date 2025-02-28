09a0351e042214474c7d7cfd8e67384caa9a26fb
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-02-28 15:39:03 +0100

    chore: use simpler notation for rating

--- REACT-TP4/src/components/Content/Home/Testimonies/Rating.tsx
+++ REACT-TP4/src/components/Content/Home/Testimonies/Rating.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Rating: React.FC<{ rating: number }> = ({ rating }) => {
+  const stars = [];
+
+  for (let i = 0; i < 5; i++) {
+    stars.push(
+      <i key={i} className={`fa-solid fa-star ${i < rating ? "text-secondary" : ""}`}></i>
+    );
+  }
+
   return (
     <span>
-      {[...Array(5)].map((_, i) => (
-        <i key={i} className={`fa-solid fa-star ${i < rating ? "text-secondary" : ""}`}></i>
-      ))}
+      {stars}
     </span>
   )
 };
